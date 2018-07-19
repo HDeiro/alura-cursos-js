6a6ec8407928109304d0f1361b1517fb0964adfc
Hugo <hugodeiro@gmail.com>
2018-07-19 11:43:04 -0300

    Implementing takeUntil and debounceTime

--- js-funcional/public/app/app.js
+++ js-funcional/public/app/app.js
@@ -1,10 +1,19 @@
 import './utils/array-helpers.js';
 import {notasService as service} from './nota/service.js';
+import {takeUntil, debounceTime, partialize, pipe} from './utils/operators.js';
+
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const operation = operations(() => 
+    service
+        .sumItems(2143)
+        .then(console.log)
+        .catch(console.log)
+);
 
 document
     .querySelector("#myButton")
-    .onclick = event => 
-        service
-            .sumItems(2143)
-            .then(console.log)
-            .catch(console.log);
+    .onclick = operation;
--- js-funcional/public/app/utils/operators.js
+++ js-funcional/public/app/utils/operators.js
@@ -3,3 +3,13 @@ export const partialize = (fn, ...args) => fn.bind(null, ...args);
 export const compose = (...fns) => value => fns.reduceRight((previous, current) => current(previous), value);
 
 export const pipe = (...fns) => value => fns.reduce((previous, current) => current(previous), value);
+
+export const takeUntil = (times, fn) => () => times-- > 0 && fn();
+
+export const debounceTime = (milliseconds, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer);
+        timer = setTimeout(fn, milliseconds);
+    }
+}
